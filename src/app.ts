1b5a9bdf023fa5ae922182dd81b420229c8c06ad
tgnc <tgnc@yandex-team.ru>
2020-03-08 00:06:44 +0300

    body parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || 5555;
 
-const app = express().use(express.json());
+const app = express()
+    .use(express.json())
+    .use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Server is started on port ${PORT}`);
